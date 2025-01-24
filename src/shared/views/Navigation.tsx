74f0de182d2c58732f998fd27569e78c15eea6cf
Anna Kohlbecker <114401864+AnnaKohlbecker@users.noreply.github.com>
2025-01-24 14:30:30 +0100

    basic navigation

--- src/shared/views/Navigation.tsx
+++ src/shared/views/Navigation.tsx
@@ -1,21 +1,56 @@
-import { StyleSheet, View } from 'react-native'
-import { Text } from 'react-native-paper'
+import React from 'react'
+import { BottomNavigation, Text } from 'react-native-paper'
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-    },
-    text: {
-        textAlign: 'center',
-    },
-})
+const MapRoute = () => <Text>Map</Text>
+
+const ProblemsRoute = () => <Text>Problems</Text>
+
+const AuthoritiesRoute = () => <Text>Authorities</Text>
+
+const ProfileRoute = () => <Text>Profile</Text>
 
 const Navigation = () => {
+    const [index, setIndex] = React.useState(0)
+    const [routes] = React.useState([
+        {
+            key: 'map',
+            title: 'Karte',
+            focusedIcon: 'map-marker',
+            unfocusedIcon: 'map-marker-outline',
+        },
+        {
+            key: 'problems',
+            title: 'Probleme',
+            focusedIcon: 'alert-circle',
+            unfocusedIcon: 'alert-circle-outline',
+        },
+        {
+            key: 'authorities',
+            title: 'Behörden',
+            focusedIcon: 'office-building',
+            unfocusedIcon: 'office-building-outline',
+        },
+        {
+            key: 'profile',
+            title: 'Profil',
+            focusedIcon: 'account',
+            unfocusedIcon: 'account-outline',
+        },
+    ])
+
+    const renderScene = BottomNavigation.SceneMap({
+        map: MapRoute,
+        problems: ProblemsRoute,
+        authorities: AuthoritiesRoute,
+        profile: ProfileRoute,
+    })
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Get started by editing ~/shared/views/Navigation.tsx</Text>
-        </View>
+        <BottomNavigation
+            navigationState={{ index, routes }}
+            onIndexChange={setIndex}
+            renderScene={renderScene}
+        />
     )
 }
 export default Navigation
