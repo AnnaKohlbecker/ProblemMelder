5dc3ffacd5ecef2f397189fb5aec450aff8a9a6e
Simon Schmieder <80328887+schmieders@users.noreply.github.com>
2025-03-19 12:25:45 +0100

    fix migration v2

---
+++ supabase/migrations/20250319111315_fix_user_function_again.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE FUNCTION "public"."insert_user_data"() RETURNS "trigger"
+    LANGUAGE "plpgsql" SECURITY DEFINER
+    AS $$BEGIN
+    INSERT INTO "public"."UserData" ("userId", role, points, name, email, "authorityId")
+    VALUES (
+        NEW.id,
+        CAST(NULLIF(NEW.raw_user_meta_data->>'role', '') AS INTEGER),
+        0,
+        NEW.raw_user_meta_data->>'name',
+        NEW.email,
+        CAST(NULLIF(NEW.raw_user_meta_data->>'authorityId', '') AS BIGINT)
+    );
+    RETURN NEW;
+END;$$;
